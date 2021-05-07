6ade2051d08c0afa1e02eade1b257c35935882b8
Francisco Rones <ininorones@gmail.com>
2021-05-07 15:58:35 -0700

    Update: item successfully added to the cart

--- gpuBot.js
+++ gpuBot.js
@@ -4,7 +4,7 @@ const puppeteer = require("puppeteer");
 // const init = document.querySelector(".start");
 
 const test_url =
-  "https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442";
+  "https://www.bestbuy.com/site/insignia-monitor-wipes-80-pack-white/8041012.p?skuId=8041012";
 
 const startBrowser = async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -13,14 +13,35 @@ const startBrowser = async () => {
   return page;
 };
 
-// const addToCart = async () => {
+const addToCart = async (page) => {
+  await page.$eval(
+    "[class='btn btn-primary btn-lg btn-block btn-leading-ficon add-to-cart-button']",
+    (element) => element.click()
+  );
+  await page.waitFor(2000);
+  await page.$eval("[class='btn btn-secondary btn-sm btn-block ']", (element) =>
+    element.click()
+  );
+  await page.waitFor(2000);
+  await page.$eval("[class='btn btn-lg btn-block btn-primary']", (element) =>
+    element.click()
+  );
+  await page.waitFor(3000);
+  await page.$eval(
+    "[class='btn btn-secondary btn-lg cia-guest-content__continue guest']",
+    (element) => element.click()
+  );
+};
 
-// }
-
-startBrowser();
 // start to start the scalping
 // init.addEventListener("click", function (e) {
 //   e.preventDefault();
 //   alert("Hello");
 //     startBrowser();
 // });
+
+const testInit = async () => {
+  const page = await startBrowser();
+  await addToCart(page);
+};
+testInit();
