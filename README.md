eb41bf1fdfc4fe35fd8085a16a9a499261cb9a60
Francisco Rones <ininorones@gmail.com>
2021-05-10 09:15:31 -0700

    Update: index and main gpuBot files are merged.

---
+++ README.md
@@ -0,0 +1,48 @@
+# Best-buy-GPUBOT
+
+### Descritption
+
+to automatically buy products posted on bestbuy as soon as it goes in stock or available for purchase.
+
+### Installation
+
+required installation
+npm i to install all node modules
+
+create .env file and add your user information(address, card information)
+
+### Usage
+
+use to buy out of stock GPU
+
+### Table of Contents
+
+[Description](###Descritption)
+
+[Installation](###Installation)
+
+[Usage](###Usage)
+
+[License](###License)
+
+[Contributing](###Contributions)
+
+[Tests](###Tests)
+
+### License
+
+License under [MIT License](License)
+
+### Contributions
+
+### Tests
+
+na
+
+### Questions and Suggestions
+
+Feel free to check out my Github:
+
+[github.com/francisN21](https://github.com/francisN21)
+
+You can reach out to me for questions and suggestions at ininorones@gmail.com
--- gpuBot.js
+++ gpuBot.js
@@ -1,12 +1,13 @@
 // import puppeteer from "puppeteer";
 const puppeteer = require("puppeteer");
+require("dotenv").config();
 
 // uncomment below once it is ready to mount as chrome extension
 // const init = document.querySelector(".start");
 
 // add URL here
 const test_url =
-  "https://www.bestbuy.com/site/endust-screen-cleaning-wipes-70-pack-blue/5611313.p?skuId=5611313";
+  "https://www.bestbuy.com/site/insignia-8-oz-cleaning-dusters-2-pack/8045009.p?skuId=8045009";
 
 // DOM needs to load and go to the product page
 const startBrowser = async () => {
@@ -19,74 +20,100 @@ const startBrowser = async () => {
   return page;
 };
 
-// add to cart
 const addToCart = async (page) => {
   await page.waitFor(3000);
-  // add item to cart
-
-  await page.$eval(
-    "[class='btn btn-primary btn-lg btn-block btn-leading-ficon add-to-cart-button']",
-    (element) => {
-      element.click();
-    }
-  );
-  await page.waitFor(2000);
-  // another page to add to go to cart
-  await page.$eval("[class='btn btn-secondary btn-sm btn-block ']", (element) =>
-    element.click()
-  );
-  await page.waitFor(2000);
-  await page.$eval("[class='btn btn-lg btn-block btn-primary']", (element) =>
-    element.click()
-  );
-  await page.waitFor(4000);
-  await page.$eval(
-    "[class='btn btn-secondary btn-lg cia-guest-content__continue guest']",
-    (element) => element.click()
-  );
-  await page.waitFor(3000);
+  try {
+    await page.$eval(
+      "[class='btn btn-primary btn-lg btn-block btn-leading-ficon add-to-cart-button']",
+      (element) => {
+        element.click();
+      }
+    );
+    await page.waitFor(2000);
+    // another page to add to go to cart
+    await page.$eval(
+      "[class='btn btn-secondary btn-sm btn-block ']",
+      (element) => element.click()
+    );
+    await page.waitFor(2000);
+    await page.$eval("[class='btn btn-lg btn-block btn-primary']", (element) =>
+      element.click()
+    );
+    await page.waitFor(4000);
+    await page.$eval(
+      "[class='btn btn-secondary btn-lg cia-guest-content__continue guest']",
+      (element) => element.click()
+    );
+    await page.waitFor(3000);
+  } catch (error) {
+    console.log(error + " out of stock");
+    await page.reload();
+    await addToCart(page);
+  }
 };
-
 const fillInfo = async (page) => {
   await page.waitFor(3000);
-  // personal info section
-  await page.type(
-    "[id='consolidatedAddresses.ui_address_2.firstName']",
-    "Francisco"
-  );
-  await page.type(
-    "[id='consolidatedAddresses.ui_address_2.lastName']",
-    "Rones"
-  );
-  await page.type(
-    "[id='consolidatedAddresses.ui_address_2.street']",
-    "2785 14th St"
-  );
-  await page.type(
-    "[id='consolidatedAddresses.ui_address_2.city']",
-    "San Pablo"
-  );
-  await page.select("[id='consolidatedAddresses.ui_address_2.state']", "CA");
-  await page.type("[id='consolidatedAddresses.ui_address_2.zipcode']", "94806");
-  await page.type("[id='user.emailAddress']", "ininorones@gmail.com");
-  await page.type("[id='user.phone']", "5106853884");
-  await page.waitFor(1000);
-  await page.$eval("[class='btn btn-lg btn-block btn-secondary']", (element) =>
-    element.click()
-  );
-  // end personal info and go to credit card page
+  console.log("filling up info");
+  try {
+    await page.waitFor(2000);
+    // personal info section
+    await page.type(
+      "[id='consolidatedAddresses.ui_address_2.firstName']",
+      process.env.NAME
+    );
+    await page.type(
+      "[id='consolidatedAddresses.ui_address_2.lastName']",
+      process.env.LASTNAME
+    );
+    await page.type(
+      "[id='consolidatedAddresses.ui_address_2.street']",
+      process.env.STADDRESS
+    );
+    await page.type(
+      "[id='consolidatedAddresses.ui_address_2.city']",
+      process.env.CITY
+    );
+    await page.select(
+      "[id='consolidatedAddresses.ui_address_2.state']",
+      process.env.STATE
+    );
+    await page.type(
+      "[id='consolidatedAddresses.ui_address_2.zipcode']",
+      process.env.ZIP
+    );
+    await page.type("[id='user.emailAddress']", process.env.EMAIL);
+    await page.type("[id='user.phone']", process.env.PHONE);
+    await page.waitFor(1000);
+    await page.$eval(
+      "[class='btn btn-lg btn-block btn-secondary']",
+      (element) => element.click()
+    );
+    // end personal info and go to credit card page
+  } catch (error) {
+    console.log("running fill info again");
+    await page.reload();
+    await fillInfo(page);
+  }
 };
 const fillCard = async (page) => {
-  await page.waitFor(5000);
-  // fake credit card info selected and checks out the item
-  await page.type("[id='optimized-cc-card-number']", "4651963205549150");
-  await page.select("[name='expiration-month']", "09");
-  await page.select("[name='expiration-year']", "2023");
-  await page.type("[id='credit-card-cvv']", "914");
-  await page.waitFor(2000);
-  await page.$eval("[class='btn btn-lg btn-block btn-primary']", (element) =>
-    element.click()
-  );
+  try {
+    await page.waitFor(4000);
+    console.log("filling up credit");
+    // fake credit card info selected and checks out the item
+    await page.type("[id='optimized-cc-card-number']", "4651963205549150");
+    await page.waitFor(1500);
+    await page.select("[name='expiration-month']", "09");
+    await page.select("[name='expiration-year']", "2023");
+    await page.type("[id='credit-card-cvv']", "914");
+    await page.waitFor(2000);
+    await page.$eval("[class='btn btn-lg btn-block btn-primary']", (element) =>
+      element.click()
+    );
+  } catch (error) {
+    console.log("running fill info again");
+    await page.reload();
+    await fillCard(page);
+  }
 };
 // start to start the scalping
 // init.addEventListener("click", function (e) {
@@ -96,9 +123,13 @@ const fillCard = async (page) => {
 // });
 
 const testInit = async () => {
-  const page = await startBrowser();
-  await addToCart(page);
-  await fillInfo(page);
-  await fillCard(page);
+  try {
+    const page = await startBrowser();
+    await addToCart(page);
+    await fillInfo(page);
+    await fillCard(page);
+  } catch (error) {
+    console.log(error);
+  }
 };
 testInit();
